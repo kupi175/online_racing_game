b98741c4d6d0e5cdb71cbc0be32a543363e4429e
peeter-virk <32205703+PeeterVirk@users.noreply.github.com>
2020-10-25 12:54:22 +0200

    Switched to threading based paralelism

--- Code/client.py
+++ Code/client.py
@@ -5,16 +5,16 @@ import socket
 # import pyglet
 import time
 
-#info about the default ip and port
+# info about the default ip and port
 version = '0.01'
-host = '139.162.136.115'
+host = '127.0.0.1'  # 139.162.136.115
 port = 12345
 
-#create the socket and form address
+# create the socket and form address
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = (host, port)
 
-#try to connect to ip:port
+# try to connect to ip:port
 try:
     socket.connect(addr)
 except Exception as e:
@@ -23,5 +23,5 @@ except Exception as e:
 timestamp = time.time()
 while True:
     print(socket.recv(1024 * 2).decode('utf-8'))
-    print(1 / (time.time() - timestamp))
+    print((time.time() - timestamp))
     timestamp = time.time()
