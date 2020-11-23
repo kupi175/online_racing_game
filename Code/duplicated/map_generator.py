48dbbcfcf5053b041eb88d6b4037a118846d336f
peeter-virk <32205703+PeeterVirk@users.noreply.github.com>
2020-11-23 15:59:14 +0200

    renamed a few lines

--- Code/duplicated/map_generator.py
+++ Code/duplicated/map_generator.py
@@ -4,14 +4,18 @@ from opensimplex import OpenSimplex
 
 class game_map:
     type = None
-    lap_count = None
-    generated_map = []
     length = None
     noise = None
+    resolution = 1
 
-    def __init__(self, seed=random.randint(0, 2147483648), type='Straight', laps=None, length=100):
+    def __init__(self, seed=random.randint(0, 2147483648), type='point_to_point', length=100, resolution = 1):
         self.noise = OpenSimplex(seed=seed)
         self.length = length
+        self.type = type
+        self.resolution = resolution
         for i in range(0, self.length):
             self.generated_map = (self.noise.noise2d(i, 0)+1)*20 # generate a string of noise
             print(self.generated_map)
+
+    def get_map(self,start,end):
+        pass
