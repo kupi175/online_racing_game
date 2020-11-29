27d36c335281426dff080ba9f2fe6565fded68bb
peeter-virk <32205703+PeeterVirk@users.noreply.github.com>
2020-11-29 14:14:51 +0200

    renamed a few lines

--- Code/client_side/client.py
+++ Code/client_side/client.py
@@ -8,11 +8,11 @@ import Code.client_side.client_networking as netw
 networking = netw.connection()
 
 time.sleep(1)
-networking.send_message(msg = 'siin')
+networking.send_message(msg='siin')
 time.sleep(1)
-networking.send_message(msg = 'on')
+networking.send_message(msg='on')
 time.sleep(1)
-networking.send_message(msg = 'hea')
+networking.send_message(msg='hea')
 while True:
-    print (networking.get_messages())
+    print(networking.get_messages())
     time.sleep(1)
