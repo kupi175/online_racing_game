a9d8f1d7767273d12b76a3ee2eaef99800bdc456
Walther Kraam <wkraam@yahoo.com>
2020-12-06 21:48:16 +0200

    Update gameObjects.py

--- W/gameObjects.py
+++ W/gameObjects.py
@@ -1,9 +1,15 @@
+'''to create game objects'''
 
-
-class playerAtributes:
-    def _init_(self, posx, posy):
+class atributes:
+    posx=0
+    posy=0
+    height=0
+    width=0
+    def _init_(self, posx, posy, height, width):
         self.posx = posx
         self.posy = posy
+        self.height = height
+        self.width = width
         self.velx = 0
         self.vely = 0
 
